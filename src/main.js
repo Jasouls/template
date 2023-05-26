abce8b48a20d1aeff511830e6ce85f4d11cde826
Jsaouls <2868780767@qq.com>
2023-05-26 21:24:56 +0800

    feat:svg公用组件封装

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installIcons from '@/icons'
 
 import 'element-plus/dist/index.css'
 
+const app = createApp(App)
+installIcons(app)
 createApp(App).use(store).use(router).mount('#app')
